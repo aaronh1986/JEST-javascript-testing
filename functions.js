262200aa02137d4fd2b88b34dad1434499f4b1b3
aaronh1986 <aaronharbourne@gmail.com>
2021-09-05 22:16:22 +0000

    28:37

--- functions.js
+++ functions.js
@@ -1,7 +1,17 @@
+const axios = require('axios')
+
 const functions = {
     add: (num1, num2) => num1 + num2,
     isNull: () => null,
-    checkValue: g => g
+    checkValue: g => g,
+    createUser: () => {
+        const user = {firstName: 'Aaron' }
+        user['lastName'] = 'Harbourne'
+        return user
+    },
+    fetchUser: () => axios.get('https://jsonplaceholder.typicode.com/users/1')
+    .then(res => res.data)
+    .catch(err => 'error')
 }
 
 module.exports = functions
--- functions.test.js
+++ functions.test.js
@@ -15,3 +15,38 @@ test('Displays null', () => {
 test('Should be falsy', () => {
     expect(functions.checkValue(null)).toBeFalsy()
 })
+
+//toEqual
+test('User should be Aaron Harbourne object', () => {
+    expect(functions.createUser()).toEqual({firstName: 'Aaron', lastName: 'Harbourne'})
+})
+
+// Less than or Greater than...Can just build the function here and not in functions.js
+test('Should be under 1600', () => {
+    const load1 = 800;
+    const load2 = 700;
+    expect(load1 + load2).toBeLessThanOrEqual(1500)
+})
+
+//Regex
+test('There is no I in team', () => {
+    expect('team').not.toMatch(/i/)
+})
+
+// Working with async data
+test('User fetched name should be LG initials', () => {
+    expect.assertions(1);
+    return functions.fetchUser()
+    .then(data => {
+      expect(data.name).toEqual('Leanne Graham')
+    })
+})
+
+// Async await, to do on Monday
+test('User fetched name should be LG initials', () => {
+    expect.assertions(1);
+    return functions.fetchUser()
+    .then(data => {
+      expect(data.name).toEqual('Leanne Graham')
+    })
+})
